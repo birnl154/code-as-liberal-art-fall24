07a9eb16cf64b1fa2a06060df0784483a1b3ceb5
birnl154 <birnl154@newschool.edu>
2024-09-04 16:00:12 -0400

    Update classwork.py

---
+++ unit-1/lesson-2/classwork.py
@@ -0,0 +1,25 @@
+score = 28
+name = "lydia"
+
+# write a lot of comments
+
+print(name)
+
+name = "bob"
+
+print(name)
+print (score)
+
+count = 0
+print(count)
+count = count+1
+print(count)
+
+
+names = ["bob", "lydia", "george", "fred"]
+numberofnames = len(names)
+print(numberofnames)
+print(names[0])
+
+for student in names:
+    print(student)
