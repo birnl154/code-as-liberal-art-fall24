682c25f147d8868647d0f5be4a8ca57102a6ca85
birnl154 <birnl154@newschool.edu>
2024-09-11 15:41:13 -0400

    Class 9/11

---
+++ unit-1/lesson-3/manovich-bot.py
@@ -0,0 +1,11 @@
+import sys 
+from manovichdata import *
+
+print (variability[0])
+
+    
+    
+
+# variability = manovichdata(statement)
+
+# print(f"Random item: {manovichdata")
---
+++ unit-1/lesson-3/manovichdata.py
@@ -0,0 +1,13 @@
+
+
+
+
+variability = ["A new media object is not a static thing with a singular meaning or representation.", "Is anything ever really new?", "New media is about versions not about originality."]
+
+automation = ["is not autonomy", "is a very old idea", "does not equal freedom from work"]
+
+numerical = ["blank1", "blank2"]
+
+modularity = ["fractal structure of new media", "the principle that can measure down to the atom"]
+
+transcoding = ["the most substantial consequence of the computerization media", "conceptual transfer"]
