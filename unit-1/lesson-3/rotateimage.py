7432b130cb1210a989ae73e70cde5a6c375eb3be
birnl154 <birnl154@newschool.edu>
2024-09-16 15:41:36 -0400

    update

--- unit-1/lesson-3/rotateimage.py
+++ unit-1/lesson-3/rotateimage.py
@@ -1,23 +1,13 @@
-
-
 import sys
 from PIL import Image
 
-if len(sys.argv) > 1:
-    image_path = sys.argv[1]
-    
-    try:
-        img = Image.open(image_path)
-        print("image loaded")
-        
-        rotated_img = img.rotate(90, expand=True)
-        
-        
-        rotated_img.save("rotated_image.jpg")
-        print("Rotated image saved as 'rotated_image.jpg'")
-    
-    except FileNotFoundError:
-        print(f"File '{image_path}' not found.")
-else:
-    print("Please provide the image file path as an argument.")
+if len(sys.argv) != 2:
+    exit("This command requires one argument: the name of an image file")
+    #if len(sys.argv) means give me all the arguments and if its not equal to 2, then i need at least 2
 
+img = Image.open( sys.argv[1] )
+
+
+rotated_img = img.rotate( int(sys.argv[2]) )\
+
+rotated_img.save("rotated-" + sys.argv[1])
