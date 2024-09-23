88942999b12cc259df7db7c7ca1ddbce6088dee1
birnl154 <birnl154@newschool.edu>
2024-09-23 11:31:05 -0400

    assignment 3

--- unit-1/lesson-3/rotateimage.py
+++ unit-1/lesson-3/rotateimage.py
@@ -1,13 +1,15 @@
 import sys
 from PIL import Image
 
-if len(sys.argv) != 2:
-    exit("This command requires one argument: the name of an image file")
-    #if len(sys.argv) means give me all the arguments and if its not equal to 2, then i need at least 2
+# if len(sys.argv) != 2:
+#     exit("This command requires one argument: the name of an image file")
+#     #if len(sys.argv) means give me all the arguments and if its not equal to 2, then i need at least 2
 
 img = Image.open( sys.argv[1] )
 
 
-rotated_img = img.rotate( int(sys.argv[2]) )\
+rotated_img = img.rotate( int(sys.argv[2]) )
 
 rotated_img.save("rotated-" + sys.argv[1])
+
+#the save will save the new image under the same folder with the rotated tag 
