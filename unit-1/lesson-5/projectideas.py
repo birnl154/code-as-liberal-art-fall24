5aeeaea66febeb1945a0272f2cd6a506db939b82
birnl154 <birnl154@newschool.edu>
2024-09-23 15:31:47 -0400

    project ideas

---
+++ unit-1/lesson-5/projectideas.py
@@ -0,0 +1,7 @@
+# first i will need to import pil image library
+# then i will have to have a folder of the different body parts for the code to choose randomly from 
+#i will then resize the images (outside of the code, just on my own) so they can be cleanly placed together
+#the images, once selected randomly, will be placed at specific coordinates depending on their body part type. ie the head will always go at (0,0) and so on
+#one will be selected from the head, torso, leg, and feet folder and assembled into one image then saved
+#it will essentially represent the randomization of how humans are made and how each human is completely unique. i want to provide many options for each body part so the user will keep getting unique results
+#advice: do not go over 1000 px for image size
