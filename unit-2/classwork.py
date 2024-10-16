4894210ee525567c65476c389b5b5c161510bbac
birnl154 <birnl154@newschool.edu>
2024-10-16 00:18:38 -0400

    classwork 10/11

---
+++ unit-2/classwork.py
@@ -0,0 +1,16 @@
+myList = []
+
+print(myList)
+
+myList.append("today")
+
+print(myList)
+
+isRaeInList = "rae" in myList
+
+print(isRaeInList)
+
+university = {}
+university["subject"] = "theory"
+university[]
+#can also make these equal to a list instead of using quotes
