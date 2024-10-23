fbaefa899fbde6097735d7bc6ad0d6c3951783b0
birnl154 <birnl154@newschool.edu>
2024-10-23 12:38:39 -0400

    Update assignment-6-2.py

--- unit-2/assignment-6-2.py
+++ unit-2/assignment-6-2.py
@@ -16,3 +16,5 @@ def getSpecificElements(soupdata, element_name):
     website_data.close()
 
 getSpecificElements(website_soup_html, ".step")
+
+#the question i asked for this data set is: what article will enhance the effect of the data txt I gathered? i thought it would then be interesting to put the biotech crop data against budgeting because i think it brings up the idea that we spend so much money on produce now because of rising prices and they are very artifical. So we are paying extremely high prices for artifical products that are sold as natural.
